d7a2370e73f19221ba699874abafc89b4dfcbf01
moaz <moaz43700@gmail.com>
2024-12-12 20:12:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Welcome to C#
+            //Region
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, Moaz");
+            //Console.ReadLine();
+            #endregion
+
+            #region Comments
+            //this is our first program in c#
+            /*
+             Multi
+             Line
+             Comment
+             */
+            #endregion
+
+            #region Naming Convention
+            //Case Sensitive
+            //Camal Case =>moazAhmed --> JS
+            //Pascal Case =>MoazAhmed --> C#
+            //Kabab_Case =>Moaz_Ahmed --> Angular
+            #endregion
+        }
+    }
+}
